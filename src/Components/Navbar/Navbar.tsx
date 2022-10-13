01d65cbaae3b6f63c8496572204a7cb6534f0b1a
Krystian Gagracz <krystian.gagracz@ec-at.com>
2022-10-13 13:34:56 +0200

    removed logo

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -6,13 +6,6 @@ import { SearchStore } from "../../Store/SearchStore";
 const Navbar: React.FC = () => {
   return (
     <header className="navbar">
-      <div className="logo-container">
-        <img
-          src="https://i.pinimg.com/736x/0d/cf/b5/0dcfb548989afdf22afff75e2a46a508.jpg"
-          alt="logo"
-          width={50}
-        />
-      </div>
       <div className="searchbar-container">
         <SearchBar searchStore={SearchStore} />
       </div>
